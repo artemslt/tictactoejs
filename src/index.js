2b9b05d310cd20244e53e4d574e6d73fa8dbd231
Artem <ppt.artem@gmail.com>
2022-08-19 12:38:15 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.com/v3.1/name/can')
+  .then(res => res.json())
+  .then(data => {
+    if (data.length > 10) {
+      alert('ddd');
+    }
+    console.log('data', data);
+  });
